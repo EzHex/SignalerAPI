d518de5b2be40248fd25eb6be6897e5781898386
Modestas Seibutis <modestas.seibutis@ktu.edu>
2023-10-04 13:15:56 +0300

    test

--- Program.cs
+++ Program.cs
@@ -3,16 +3,6 @@ using TowerinoSignaler;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
-// Listen on all IP addresses and port 5235
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(5235);
-});
-// Listen on a specific IP address and port 5235
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.Listen(IPAddress.Parse("158.129.1.136"), 5235);
-});
 
 var app = builder.Build();
 
