e303a0a46e1d819fd1f98c1386fefdc89fcf53c7
Arnas B <43886777+EzHex@users.noreply.github.com>
2023-10-05 16:53:26 +0300

    removed auto browser.

--- obj/Debug/net8.0/TowerinoSignaler.AssemblyInfo.cs
+++ obj/Debug/net8.0/TowerinoSignaler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TowerinoSignaler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bda26fe300f4dafccf3f0b99acdfb66948e3c072")]
 [assembly: System.Reflection.AssemblyProductAttribute("TowerinoSignaler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TowerinoSignaler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
